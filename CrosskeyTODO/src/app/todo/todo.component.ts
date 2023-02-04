cd7c33ca04b04f04bc9054059ee2ae38e2444fc9
Merriz <77438758+Merriz@users.noreply.github.com>
2023-02-04 17:53:39 +0200

    Added lots of modules from Angular Material

--- CrosskeyTODO/src/app/todo/todo.component.ts
+++ CrosskeyTODO/src/app/todo/todo.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Task } from '../model/task';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
+  todoForm!: FormGroup;
+  tasks: Task[] = [];
+  inProgress: Task[] = [];
+  done: Task[] = [];
+  constructor(private fb: FormBuilder) {}
 
+  ngOnInit(): void {
+    this.todoForm = this.fb.group({
+      item: ['', Validators.required],
+    });
+  }
+
+  drop(event: CdkDragDrop<Task[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 }
